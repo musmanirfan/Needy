f6eebb816572dc8c831a53b7483316bf558cbaba
musmanirfan <usmanmala356@gmail.com>
2024-12-04 04:54:38 +0500

    dynamic routing page ui completed

--- src/app/company/job/[slug]/page.tsx
+++ src/app/company/job/[slug]/page.tsx
@@ -45,29 +45,33 @@ export default function Page() {
             <CompanyHeader />
             {!loading ? (
 
-                <div className='flex justify-center mt-32'>
+                <div className='flex justify-center mt-32 mb-10'>
                     {
                         data ? (
-                            <div className=" !w-[700px] overflow-x-hidden">
+                            <div className=" !w-[1000px] overflow-x-hidden">
                                 <div className="bg-white shadow-lg rounded-lg !w-[100%] overflow-x-hidden">
-                                    <Image
-                                        className="object-cover w-[100%] sm:w-full h-64"
-                                        src={data.companyLogo!}
-                                        alt={data.companyName!}
-                                        width={800}
-                                        height={400}
-                                        unoptimized
-                                    />
+                                    <div className='flex items-center gap-3'>
+                                        <Image
+                                            className="object-cover w-[150px] h-[150px] rounded-full border"
+                                            src={data.companyLogo!}
+                                            alt={data.companyName!}
+                                            width={120}
+                                            height={120}
+                                            unoptimized
+                                        />
+                                        <div>
+                                            <h1 className="text-3xl font-bold">{data.companyName}</h1>
+                                            {data.jobType && (
+                                                <div className="">
+                                                    <span className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                                                        {data.jobType}
+                                                    </span>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
                                     <div className="p-6">
-                                        <h1 className="text-3xl font-bold mb-4">{data.companyName}</h1>
                                         {/* <p className="text-sm text-gray-500 mb-2">{formatDate(data)}</p> */}
-                                        {data.jobType && (
-                                            <div className="mb-4">
-                                                <span className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
-                                                    {data.jobType}
-                                                </span>
-                                            </div>
-                                        )}
                                         <div className="text-gray-700 leading-relaxed prose">
                                             <ReactMarkdown>
                                                 {data.otherReq}
